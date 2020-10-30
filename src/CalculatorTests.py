5335bd61b806f3bf77f5cf1f2d0a486412b0e934
Erik Dannenberg <ed262@njit.edu>
2020-10-30 12:30:30 -0400

    Feature: setup calculator class and test

---
+++ src/CalculatorTests.py
@@ -0,0 +1,12 @@
+import unittest
+from Calculator import Calculator
+
+class MyTestCase(unittest.TestCase):
+
+    def test_instantiate_calculator(self):
+        calculator = Calculator()
+        self.assertIsInstance(calculator, Calculator)
+
+
+if _name_ == '_main_':
+    unittest.main()
